7f5b92ab7bdee15172b2e795bad411e068666918
Matheus Serafim <matheus18serafim@gmail.com>
2021-10-05 15:47:39 -0300

    Finalizado a troca de usuário

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,21 +28,65 @@ const criandoTabuleiro = () => {
 }
 criandoTabuleiro()
 
+let contbloco1 = 1
+let contbloco2 = 1
+let contbloco3 = 1
+let contbloco4 = 1
+let contbloco5 = 1
+let contbloco6 = 1
+let contbloco7 = 1
+let contColor = 0
+let controlDiscs = true
 const addDiscos = (evt) => {
+    let discColor = ['disco-azul','disco-red']
     let discSelected = evt.target
-    let select = discSelected.closest('div #bloco-1')
+    let select = discSelected.closest(`div .blocos`)    
+    let discoAzul = document.createElement('div')
+    discoAzul.classList.add(`${discColor[contColor]}`)
+    
 
-    // console.log(discSelected)
-    if (select.id === 'bloco-1'){
-        disco = document.createElement('div')
-        disco.classList.add('disco-azul')
-        discSelect = select.firstElementChild
-        console.log(discSelect)
-        console.log(discSelect.className)
-        if (discSelect.className === discSelect.className){
-            discSelect.appendChild(disco)
-        }
-        
+    if(controlDiscs === true){
+        discoAzul.classList.add(`${discColor[0]}`)
+        controlDiscs = false
+    }else{
+        discoAzul.classList.add(`${discColor[1]}`)
+        controlDiscs = true
+    }
+
+    if (select.id === 'bloco-1'){  
+        discSelect = select.querySelector(`div:nth-child(${contbloco1})`)
+        discSelect.appendChild(discoAzul)
+        contbloco1++
+
+    }else if (select.id === 'bloco-2'){
+        discSelect = select.querySelector(`div:nth-child(${contbloco2})`)
+        discSelect.appendChild(discoAzul)
+        contbloco2++  
+    }
+    if (select.id === 'bloco-3'){
+        discSelect = select.querySelector(`div:nth-child(${contbloco3})`)
+        discSelect.appendChild(discoAzul)
+        contbloco3++  
+    }
+    if (select.id === 'bloco-4'){
+        discSelect = select.querySelector(`div:nth-child(${contbloco4})`)
+        discSelect.appendChild(discoAzul)
+        contbloco4++  
+    }
+    if (select.id === 'bloco-5'){
+        discSelect = select.querySelector(`div:nth-child(${contbloco5})`)
+        discSelect.appendChild(discoAzul)
+        contbloco5++  
+    }
+    if (select.id === 'bloco-6'){
+        discSelect = select.querySelector(`div:nth-child(${contbloco6})`)
+        discSelect.appendChild(discoAzul)
+        contbloco6++  
+    }
+    if (select.id === 'bloco-7'){
+        discSelect = select.querySelector(`div:nth-child(${contbloco7})`)
+        discSelect.appendChild(discoAzul)
+        contbloco7++  
     }
 }
 

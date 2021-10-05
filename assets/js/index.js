9abb54315968a8990b99104fb6d6fd5d0831a377
Gustavo <ribeiro.gustavo95@gmail.com>
2021-10-05 20:11:52 -0300

    mudança de jogador corrigido

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,15 +11,15 @@ let map = [
 ]
 
 const criandoTabuleiro = () => {
-    for (let i = 0; i < map.length; i++){
+    for (let i = 0; i < map.length; i++) {
         let blocos = document.createElement('div')
-        blocos.setAttribute('id', `bloco-${i+1}`)
+        blocos.setAttribute('id', `bloco-${i + 1}`)
         blocos.classList.add('blocos')
-        for (let j = 0; j < map[i].length; j++){
+        for (let j = 0; j < map[i].length; j++) {
             let linhas = document.createElement('div')
-            if (map[i][j] === 'B'){
+            if (map[i][j] === 'B') {
                 linhas.classList.add('linha')
-                linhas.setAttribute('id', `${j+1}`)
+                linhas.setAttribute('id', `${j + 1}`)
                 blocos.appendChild(linhas)
             }
         }
@@ -37,57 +37,61 @@ let contbloco6 = 1
 let contbloco7 = 1
 let contColor = 0
 let controlDiscs = true
+
 const addDiscos = (evt) => {
-    let discColor = ['disco-azul','disco-red']
+    let discColor = ['disco-azul', 'disco-red']
     let discSelected = evt.target
-    let select = discSelected.closest(`div .blocos`)    
-    let discoAzul = document.createElement('div')
-    discoAzul.classList.add(`${discColor[contColor]}`)
-    
+    let select = discSelected.closest(`div .blocos`)
+    let disco = document.createElement('div')
+    disco.classList.add(`${discColor[contColor]}`)
 
-    if(controlDiscs === true){
-        discoAzul.classList.add(`${discColor[0]}`)
-        controlDiscs = false
-    }else{
-        discoAzul.classList.add(`${discColor[1]}`)
-        controlDiscs = true
-    }
-
-    if (select.id === 'bloco-1'){  
+    if (select.id === 'bloco-1' && contbloco1 < 7) {
+        changePlayer(disco, discColor)
         discSelect = select.querySelector(`div:nth-child(${contbloco1})`)
-        discSelect.appendChild(discoAzul)
+        discSelect.appendChild(disco)
         contbloco1++
 
-    }else if (select.id === 'bloco-2'){
+    } else if (select.id === 'bloco-2' && contbloco2 < 7) {
+        changePlayer(disco, discColor)
         discSelect = select.querySelector(`div:nth-child(${contbloco2})`)
-        discSelect.appendChild(discoAzul)
-        contbloco2++  
-    }
-    if (select.id === 'bloco-3'){
+        discSelect.appendChild(disco)
+        contbloco2++
+    } else if (select.id === 'bloco-3' && contbloco3 < 7) {
+        changePlayer(disco, discColor)
         discSelect = select.querySelector(`div:nth-child(${contbloco3})`)
-        discSelect.appendChild(discoAzul)
-        contbloco3++  
-    }
-    if (select.id === 'bloco-4'){
+        discSelect.appendChild(disco)
+        contbloco3++
+    } else if (select.id === 'bloco-4' && contbloco4 < 7) {
+        changePlayer(disco, discColor)
         discSelect = select.querySelector(`div:nth-child(${contbloco4})`)
-        discSelect.appendChild(discoAzul)
-        contbloco4++  
-    }
-    if (select.id === 'bloco-5'){
+        discSelect.appendChild(disco)
+        contbloco4++
+    } else if (select.id === 'bloco-5' && contbloco5 < 7) {
+        changePlayer(disco, discColor)
         discSelect = select.querySelector(`div:nth-child(${contbloco5})`)
-        discSelect.appendChild(discoAzul)
-        contbloco5++  
-    }
-    if (select.id === 'bloco-6'){
+        discSelect.appendChild(disco)
+        contbloco5++
+    } else if (select.id === 'bloco-6' && contbloco6 < 7) {
+        changePlayer(disco, discColor)
         discSelect = select.querySelector(`div:nth-child(${contbloco6})`)
-        discSelect.appendChild(discoAzul)
-        contbloco6++  
-    }
-    if (select.id === 'bloco-7'){
+        discSelect.appendChild(disco)
+        contbloco6++
+    } else if (select.id === 'bloco-7' && contbloco7 < 7) {
+        changePlayer(disco, discColor)
         discSelect = select.querySelector(`div:nth-child(${contbloco7})`)
-        discSelect.appendChild(discoAzul)
-        contbloco7++  
+        discSelect.appendChild(disco)
+        contbloco7++
     }
 }
 
 mainGame.addEventListener('click', addDiscos)
+
+function changePlayer(disco, discColor) {
+    if (controlDiscs === true) {
+        disco.classList.add(`${discColor[0]}`)
+        controlDiscs = false
+    } else {
+        disco.classList.add(`${discColor[1]}`)
+        controlDiscs = true
+    }
+}

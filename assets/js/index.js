9b586f8cf92c9a894fd3d657554984efa17edc3e
Matheus Serafim <matheus18serafim@gmail.com>
2021-10-06 10:36:37 -0300

    Melhorias na troca do jogador.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,25 @@
 const mainGame = document.querySelector('.main-game')
 
 let map = [
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0],
-    [0,0,0,0,0,0]
+    'BBBBBB',
+    'BBBBBB',
+    'BBBBBB',
+    'BBBBBB',
+    'BBBBBB',
+    'BBBBBB',
+    'BBBBBB'
 ]
 
 const criandoTabuleiro = () => {
-    for (let i = 0; i < map.length; i++) {
+    for (let i = 0; i < map.length; i++){
         let blocos = document.createElement('div')
-        blocos.setAttribute('id', `bloco-${i + 1}`)
+        blocos.setAttribute('id', `bloco-${i+1}`)
         blocos.classList.add('blocos')
-        for (let j = 0; j < map[i].length; j++) {
+        for (let j = 0; j < map[i].length; j++){
             let linhas = document.createElement('div')
-            if (map[i][j] === 0) {
+            if (map[i][j] === 'B'){
                 linhas.classList.add('linha')
-                linhas.setAttribute('id', `${j + 1}`)
+                linhas.setAttribute('id', `${j+1}`)
                 blocos.appendChild(linhas)
             }
         }
@@ -27,62 +28,67 @@ const criandoTabuleiro = () => {
 }
 criandoTabuleiro()
 
-let count = [1,1,1,1,1,1,1]
+let contbloco1 = 1
+let contbloco2 = 1
+let contbloco3 = 1
+let contbloco4 = 1
+let contbloco5 = 1
+let contbloco6 = 1
+let contbloco7 = 1
 let contColor = 0
+
 let controlDiscs = true
-
-function validaDisco (select, disco, count, i){
-    discSelect = select.querySelector(`div:nth-child(${count[i]})`)
-    discSelect.appendChild(disco)
-    count[i]++
-}
-
 const addDiscos = (evt) => {
-    let discColor = ['disco-azul', 'disco-red']
+    let discColor = ['disco-azul','disco-red']
     let discSelected = evt.target
-    let select = discSelected.closest(`div .blocos`)
-    let disco = document.createElement('div')
-    disco.classList.add(`${discColor[contColor]}`)
+    let select = discSelected.closest(`div .blocos`)    
+    let discoAzul = document.createElement('div')
+    discoAzul.classList.add(`${discColor[contColor]}`)
+    
 
-    if (select.id === 'bloco-1' && count[0] < 7) {
-        changePlayer(disco, discColor)
-        validaDisco(select, disco, count, 0)
+    if(controlDiscs === true){
+        discoAzul.classList.add(`${discColor[0]}`)
+        controlDiscs = false
+    }else{
+        discoAzul.classList.add(`${discColor[1]}`)
+        controlDiscs = true
+    }
 
-    } else if (select.id === 'bloco-2' && count[1] < 7) {
-        changePlayer(disco, discColor)
-        validaDisco(select, disco, count, 1)
+    if (select.id === 'bloco-1'){  
+        discSelect = select.querySelector(`div:nth-child(${contbloco1})`)
+        discSelect.appendChild(discoAzul)
+        contbloco1++
 
-    } else if (select.id === 'bloco-3' && count[2] < 7) {
-        changePlayer(disco, discColor)
-        validaDisco(select, disco, count, 2)
-        
-    } else if (select.id === 'bloco-4' && count[3] < 7) {
-        changePlayer(disco, discColor)
-        validaDisco(select, disco, count, 3)
-
-    } else if (select.id === 'bloco-5' && count[4] < 7) {
-        changePlayer(disco, discColor)
-        validaDisco(select, disco, count, 4)
-
-    } else if (select.id === 'bloco-6' && count[5] < 7) {
-        changePlayer(disco, discColor)
-        validaDisco(select, disco, count, 5)
-
-    } else if (select.id === 'bloco-7' && count[6] < 7) {
-        changePlayer(disco, discColor)
-        validaDisco(select, disco, count, 6)
-
+    }else if (select.id === 'bloco-2'){
+        discSelect = select.querySelector(`div:nth-child(${contbloco2})`)
+        discSelect.appendChild(discoAzul)
+        contbloco2++  
+    }
+    if (select.id === 'bloco-3'){
+        discSelect = select.querySelector(`div:nth-child(${contbloco3})`)
+        discSelect.appendChild(discoAzul)
+        contbloco3++  
+    }
+    if (select.id === 'bloco-4'){
+        discSelect = select.querySelector(`div:nth-child(${contbloco4})`)
+        discSelect.appendChild(discoAzul)
+        contbloco4++  
+    }
+    if (select.id === 'bloco-5'){
+        discSelect = select.querySelector(`div:nth-child(${contbloco5})`)
+        discSelect.appendChild(discoAzul)
+        contbloco5++  
+    }
+    if (select.id === 'bloco-6'){
+        discSelect = select.querySelector(`div:nth-child(${contbloco6})`)
+        discSelect.appendChild(discoAzul)
+        contbloco6++  
+    }
+    if (select.id === 'bloco-7'){
+        discSelect = select.querySelector(`div:nth-child(${contbloco7})`)
+        discSelect.appendChild(discoAzul)
+        contbloco7++  
     }
 }
 
 mainGame.addEventListener('click', addDiscos)
-
-function changePlayer(disco, discColor) {
-    if (controlDiscs === true) {
-        disco.classList.add(`${discColor[0]}`)
-        controlDiscs = false
-    } else {
-        disco.classList.add(`${discColor[1]}`)
-        controlDiscs = true
-    }
-}
